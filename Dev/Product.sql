ea78d067c256b3721586bc36c5af5eb24a2cc4d6
lzapanta <luzzapanta21@gmail.com>
2022-11-18 18:07:15 -0500

    Added 3 more products (cookies1, cookies2, cake1)

--- Dev/Product.sql
+++ Dev/Product.sql
@@ -90,6 +90,26 @@
 			quantityOnHand:100
 		}
 
+		{
+			ingredient_id: ObjectId("baking powder")
+			ingredientNumber: "ING-001"
+			ingredientName: "baking powder"
+			qty: 40
+			cost:.80
+			price:3.50
+			quantityOnHand:70
+		}
+
+		{
+			ingredient_id: ObjectId("baking soda")
+			ingredientNumber: "ING-001"
+			ingredientName: "baking soda"
+			qty: 80
+			cost:.80
+			price:2.99
+			quantityOnHand:80
+		}
+
 	]
 }
 
@@ -183,6 +203,392 @@
 			price:2.50
 			quantityOnHand:100
 		}
+
+		{
+			ingredient_id: ObjectId("baking powder")
+			ingredientNumber: "ING-001"
+			ingredientName: "baking powder"
+			qty: 40
+			cost:.80
+			price:3.50
+			quantityOnHand:70
+		}
+
+		{
+			ingredient_id: ObjectId("baking soda")
+			ingredientNumber: "ING-001"
+			ingredientName: "baking soda"
+			qty: 80
+			cost:.80
+			price:2.99
+			quantityOnHand:80
+		}
+
+	]
+}
+
+{
+	prod_id: ObjectId("Cookies1 ")
+	productNumber: 003
+	productName: "Dark Choco Chip Cookies"
+	productDescription: "All-time Snacks"
+	productCategory: "Snacks"
+	productRating:5
+	price:3.00
+	quantityOnHand:200
+	isOffered:1
+	ingredients: [
+		{
+			ingredient_id: ObjectId("flour")
+			ingredientNumber: "ING-001"
+			ingredientName: "flour"
+			qty: 80
+			cost:.80
+			price:2.50
+			quantityOnHand:80
+		}
+
+		{
+			ingredient_id: ObjectId("milk")
+			ingredientNumber: "ING-001"
+			ingredientName: "milk"
+			qty: 70
+			cost:.80
+			price:2.99
+			quantityOnHand:200
+		}
+
+		{
+			ingredient_id: ObjectId("yeast")
+			ingredientNumber: "yeast-001"
+			ingredientName: "flour"
+			qty: 100
+			cost:.80
+			price:2.50
+			quantityOnHand:100
+		}
+
+		{
+			ingredient_id: ObjectId("egg")
+			ingredientNumber: "ING-001"
+			ingredientName: "egg"
+			qty: 100
+			cost:.80
+			price:2.50
+			quantityOnHand:100
+		}
+
+		{
+			ingredient_id: ObjectId("sugar")
+			ingredientNumber: "ING-001"
+			ingredientName: "sugar"
+			qty: 100
+			cost:.80
+			price:2.50
+			quantityOnHand:100
+		}
+		
+		{
+			ingredient_id: ObjectId("water")
+			ingredientNumber: "ING-001"
+			ingredientName: "water"
+			qty: 100
+			cost:.80
+			price:2.50
+			quantityOnHand:100
+		}
+		
+		{
+			ingredient_id: ObjectId("vanilla")
+			ingredientNumber: "ING-001"
+			ingredientName: "vanilla"
+			qty: 100
+			cost:.80
+			price:2.50
+			quantityOnHand:100
+		}
+
+		{
+			ingredient_id: ObjectId("chocolates")
+			ingredientNumber: "ING-001"
+			ingredientName: "chocolates"
+			qty: 50
+			cost:.70
+			price:1.99
+			quantityOnHand:300
+		}
+
+		{
+			ingredient_id: ObjectId("baking powder")
+			ingredientNumber: "ING-001"
+			ingredientName: "baking powder"
+			qty: 40
+			cost:.80
+			price:3.50
+			quantityOnHand:70
+		}
+
+		{
+			ingredient_id: ObjectId("baking soda")
+			ingredientNumber: "ING-001"
+			ingredientName: "baking soda"
+			qty: 80
+			cost:.80
+			price:2.99
+			quantityOnHand:80
+		}
+
+		
+
+		
+
+	]
+}
+
+{
+	prod_id: ObjectId("Cookies2")
+	productNumber: 003
+	productName: "Macadamia Cookies"
+	productDescription: "All-time Snacks"
+	productCategory: "Snacks"
+	productRating:5
+	price:3.00
+	quantityOnHand:200
+	isOffered:1
+	ingredients: [
+		{
+			ingredient_id: ObjectId("flour")
+			ingredientNumber: "ING-001"
+			ingredientName: "flour"
+			qty: 80
+			cost:.80
+			price:2.50
+			quantityOnHand:80
+		}
+
+		{
+			ingredient_id: ObjectId("milk")
+			ingredientNumber: "ING-001"
+			ingredientName: "milk"
+			qty: 70
+			cost:.80
+			price:2.99
+			quantityOnHand:200
+		}
+
+		{
+			ingredient_id: ObjectId("yeast")
+			ingredientNumber: "yeast-001"
+			ingredientName: "flour"
+			qty: 100
+			cost:.80
+			price:2.50
+			quantityOnHand:100
+		}
+
+		{
+			ingredient_id: ObjectId("egg")
+			ingredientNumber: "ING-001"
+			ingredientName: "egg"
+			qty: 100
+			cost:.80
+			price:2.50
+			quantityOnHand:100
+		}
+
+		{
+			ingredient_id: ObjectId("sugar")
+			ingredientNumber: "ING-001"
+			ingredientName: "sugar"
+			qty: 100
+			cost:.80
+			price:2.50
+			quantityOnHand:100
+		}
+		
+		{
+			ingredient_id: ObjectId("water")
+			ingredientNumber: "ING-001"
+			ingredientName: "water"
+			qty: 100
+			cost:.80
+			price:2.50
+			quantityOnHand:100
+		}
+		
+		{
+			ingredient_id: ObjectId("vanilla")
+			ingredientNumber: "ING-001"
+			ingredientName: "vanilla"
+			qty: 100
+			cost:.80
+			price:2.50
+			quantityOnHand:100
+		}
+
+		{
+			ingredient_id: ObjectId("chocolates")
+			ingredientNumber: "ING-001"
+			ingredientName: "chocolates"
+			qty: 50
+			cost:.70
+			price:1.99
+			quantityOnHand:300
+		}
+
+		{
+			ingredient_id: ObjectId("baking powder")
+			ingredientNumber: "ING-001"
+			ingredientName: "baking powder"
+			qty: 40
+			cost:.80
+			price:3.50
+			quantityOnHand:70
+		}
+
+		{
+			ingredient_id: ObjectId("baking soda")
+			ingredientNumber: "ING-001"
+			ingredientName: "baking soda"
+			qty: 80
+			cost:.80
+			price:2.99
+			quantityOnHand:80
+		}
+
+		{
+			ingredient_id: ObjectId("Macadamia Cookies")
+			ingredientNumber: "ING-001"
+			ingredientName: "Macadamia Cookies"
+			qty: 80
+			cost:.80
+			price:2.99
+			quantityOnHand:80
+		}	
+
+	]
+}
+
+{
+	prod_id: ObjectId("Carrot Cake")
+	productNumber: 003
+	productName: "Carrot Cake"
+	productDescription: "Sweet Treats"
+	productCategory: "Cakes"
+	productRating:5
+	price:3.00
+	quantityOnHand:200
+	isOffered:1
+	ingredients: [
+		{
+			ingredient_id: ObjectId("flour")
+			ingredientNumber: "ING-001"
+			ingredientName: "flour"
+			qty: 80
+			cost:.80
+			price:2.50
+			quantityOnHand:80
+		}
+
+		{
+			ingredient_id: ObjectId("milk")
+			ingredientNumber: "ING-001"
+			ingredientName: "milk"
+			qty: 70
+			cost:.80
+			price:2.99
+			quantityOnHand:200
+		}
+
+		{
+			ingredient_id: ObjectId("yeast")
+			ingredientNumber: "yeast-001"
+			ingredientName: "flour"
+			qty: 100
+			cost:.80
+			price:2.50
+			quantityOnHand:100
+		}
+
+		{
+			ingredient_id: ObjectId("egg")
+			ingredientNumber: "ING-001"
+			ingredientName: "egg"
+			qty: 100
+			cost:.80
+			price:2.50
+			quantityOnHand:100
+		}
+
+		{
+			ingredient_id: ObjectId("sugar")
+			ingredientNumber: "ING-001"
+			ingredientName: "sugar"
+			qty: 100
+			cost:.80
+			price:2.50
+			quantityOnHand:100
+		}
+		
+		{
+			ingredient_id: ObjectId("water")
+			ingredientNumber: "ING-001"
+			ingredientName: "water"
+			qty: 100
+			cost:.80
+			price:2.50
+			quantityOnHand:100
+		}
+		
+		{
+			ingredient_id: ObjectId("vanilla")
+			ingredientNumber: "ING-001"
+			ingredientName: "vanilla"
+			qty: 100
+			cost:.80
+			price:2.50
+			quantityOnHand:100
+		}
+
+		{
+			ingredient_id: ObjectId("chocolates")
+			ingredientNumber: "ING-001"
+			ingredientName: "chocolates"
+			qty: 50
+			cost:.70
+			price:1.99
+			quantityOnHand:300
+		}
+
+		{
+			ingredient_id: ObjectId("baking powder")
+			ingredientNumber: "ING-001"
+			ingredientName: "baking powder"
+			qty: 40
+			cost:.80
+			price:3.50
+			quantityOnHand:70
+		}
+
+		{
+			ingredient_id: ObjectId("baking soda")
+			ingredientNumber: "ING-001"
+			ingredientName: "baking soda"
+			qty: 80
+			cost:.80
+			price:2.99
+			quantityOnHand:80
+		}
+
+		{
+			ingredient_id: ObjectId("carrots")
+			ingredientNumber: "ING-001"
+			ingredientName: "carrot"
+			qty: 20
+			cost:.80
+			price:2.50
+			quantityOnHand:60
+		}	
 
 	]
 }
