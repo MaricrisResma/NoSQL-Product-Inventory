003fd5aec67d951a576ba48f498be85496b7d906
lzapanta <luzzapanta21@gmail.com>
2022-11-18 17:34:56 -0500

    Added Rolls

--- Dev/Product.sql
+++ Dev/Product.sql
@@ -93,3 +93,98 @@
 	]
 }
 
+{
+	prod_id: ObjectId("Rolls")
+	productNumber: 002
+	productName: "Rolls"
+	productDescription: "All-time favorite"
+	productCategory:All-time
+	productRating:4
+	price:2.99
+	quantityOnHand:80
+	isOffered:1
+	ingredients: [
+		{
+			ingredient_id: ObjectId("flour")
+			ingredientNumber: "ING-001"
+			ingredientName: "flour"
+			qty: 80
+			cost:.80
+			price:2.50
+			quantityOnHand:80
+		}
+
+		{
+			ingredient_id: ObjectId("milk")
+			ingredientNumber: "ING-001"
+			ingredientName: "milk"
+			qty: 70
+			cost:.80
+			price:2.99
+			quantityOnHand:200
+		}
+
+		{
+			ingredient_id: ObjectId("yeast")
+			ingredientNumber: "yeast-001"
+			ingredientName: "flour"
+			qty: 100
+			cost:.80
+			price:2.50
+			quantityOnHand:100
+		}
+
+		{
+			ingredient_id: ObjectId("egg")
+			ingredientNumber: "ING-001"
+			ingredientName: "egg"
+			qty: 100
+			cost:.80
+			price:2.50
+			quantityOnHand:100
+		}
+
+		{
+			ingredient_id: ObjectId("sugar")
+			ingredientNumber: "ING-001"
+			ingredientName: "sugar"
+			qty: 100
+			cost:.80
+			price:2.50
+			quantityOnHand:100
+		}
+		
+		{
+			ingredient_id: ObjectId("water")
+			ingredientNumber: "ING-001"
+			ingredientName: "water"
+			qty: 100
+			cost:.80
+			price:2.50
+			quantityOnHand:100
+		}
+		
+		{
+			ingredient_id: ObjectId("vanilla")
+			ingredientNumber: "ING-001"
+			ingredientName: "vanilla"
+			qty: 100
+			cost:.80
+			price:2.50
+			quantityOnHand:100
+		}
+
+		{
+			ingredient_id: ObjectId("salt")
+			ingredientNumber: "ING-001"
+			ingredientName: "salt"
+			qty: 100
+			cost:.80
+			price:2.50
+			quantityOnHand:100
+		}
+
+	]
+}
+
+
